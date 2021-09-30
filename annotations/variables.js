0c1a3bdeae7c3201686313b8d234846567c918fa
jscodebit <soujanya.janapatla@gmail.com>
2021-09-30 13:25:13 -0400

    variables using annotations

---
+++ annotations/variables.js
@@ -0,0 +1,8 @@
+var apple = 5;
+var speed = 'fast';
+var hasName = true;
+var nothingMuch = null;
+var nothing = undefined;
+// built in objects
+var now = new Date();
+console.log(now);
---
+++ annotations/variables.ts
@@ -0,0 +1,21 @@
+let apple: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+console.log(now);
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2,3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {
+
+}
+
+let car = new Car();
